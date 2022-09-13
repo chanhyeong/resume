7a86cf3e42805660545c0ad18fa702eb980f8f77
조찬형[Plasma] <chu7825@gmail.com>
2022-09-13 10:20:46 +0900

    연도 추가, 렌더링 실패 글자 수정

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
+FILE_NAME_ARG=$1
+FILE_NAME=${FILE_NAME_ARG:=resume.pdf}
+
 # make pdf
-asciidoctor-pdf -a scripts=cjk -a pdf-theme=./resources/themes/cjk-theme.yml -a pdf-fontsdir="./resources/fonts;GEM_FONTS_DIR" resume.adoc
+asciidoctor-pdf -a scripts=cjk -a pdf-theme=./resources/themes/cjk-theme.yml -a pdf-fontsdir="./resources/fonts;GEM_FONTS_DIR" resume.adoc -o $FILE_NAME
